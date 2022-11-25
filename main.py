5da7ebd3178c2f86d84a8587daca238f8c0ac98a
sotaart <sotaart@yandex.ru>
2022-11-25 12:57:55 +0300

    Серега опытный мужик

---
+++ main.py
@@ -0,0 +1,37 @@
+# Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
+
+# Пример:
+
+# - 6782 -> 23
+# - 0,56 -> 11
+
+#sum = 0
+#input_num = input('Введите число: ')
+
+#for a in input_num:
+#    if a.isdigit():
+#        sum += int(a)
+
+#print(sum)
+
+
+
+#Задайте список из n чисел последовательности (1 + 1/n)^n. Вывестив консоль сам список и сумму его элементов.
+
+from msilib import sequence
+
+#n = int(input('Введите число: '))
+
+#def get_squerence(n):
+#    return [round((1 + 1 / x)**x, 5) for x in range (1, n + 1)]
+
+#nums = get_squerence(n)
+#print(nums)
+#print(round(sum(nums), 5))
+
+#Реализуйте алгоритм перемешивания списка. Встроенный алгоритм SHUFFLE не использовать! Реализовать свой метод
+#import random
+#lst = [random.randint(0,10) for i in range(random.randint(5,20))]
+#print(f"исходный список:\n {lst}")
+#random.shuffle(lst)
+#print(f"список после перемешивания:\n{lst}")
